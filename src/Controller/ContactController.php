a9f8f566643b3e25e5116f62b7dfaeb5b2207e2a
[bea] <[1@gmail.com]>
2020-11-24 09:43:42 +0100

    form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Contacto;
+use App\Form\ContactoType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,11 +14,14 @@ class ContactController extends AbstractController
      */
     public function index(Request $request,SessionInterface $session)
     {
+        $contacto=new Contacto();
+        $form= $this->createForm(ContactoType::class, $contacto);
         $usuario = $request->request->get('usuario');
         $usuario = $session->get('usuario');
         return $this->render('contact.html.twig', [
             'controller_name' => 'ContactController',
-            'usuario' => strlen($usuario)>0?$usuario. " estas contectad@":"Introduce tus datos: "
+            'usuario' => strlen($usuario)>0?$usuario. " estas contectad@":"Introduce tus datos: ",
+            'form' =>$form->createView()
         ]);
     }
 }
--- src/Controller/ProductosController.php
+++ src/Controller/ProductosController.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\Categoria;
-use App\Entity\Productos;
+use App\Entity\{Categoria,Productos};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
